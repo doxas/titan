827800df54b8423dc05467c57ea0a708a047c389
doxas <eizanco@gmail.com>
2021-10-06 22:11:37 +0900

    add config files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    titan: './titan.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      }, {
+        test: /\.(vert|frag|comp)$/,
+        use: 'raw-loader',
+      }
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9999,
+  },
+};
