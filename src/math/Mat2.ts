902152a3dfa206aae64b0eb29d28777745abe02f
doxas <eizanco@gmail.com>
2021-10-14 00:27:02 +0900

    fix matrix calculation

--- src/math/Mat2.ts
+++ src/math/Mat2.ts
@@ -12,14 +12,14 @@ export class Mat2 {
 
   /** getter ================================================================ */
   get m00(): number {return this.value[0];}
-  get m10(): number {return this.value[1];}
-  get m01(): number {return this.value[2];}
+  get m01(): number {return this.value[1];}
+  get m10(): number {return this.value[2];}
   get m11(): number {return this.value[3];}
 
   /** setter ================================================================ */
   set m00(v: number) {this.value[0] = v;}
-  set m10(v: number) {this.value[1] = v;}
-  set m01(v: number) {this.value[2] = v;}
+  set m01(v: number) {this.value[1] = v;}
+  set m10(v: number) {this.value[2] = v;}
   set m11(v: number) {this.value[3] = v;}
 
   /** property ============================================================== */
@@ -28,96 +28,96 @@ export class Mat2 {
   /** constructor =========================================================== */
   constructor(
     m00: number = 0.0,
-    m10: number = 0.0,
     m01: number = 0.0,
+    m10: number = 0.0,
     m11: number = 0.0,
   ) {
-    this.set(m00, m10, m01, m11);
+    this.set(m00, m01, m10, m11);
   }
 
   /** chain method ========================================================== */
   set(
     m00: number,
-    m10: number,
     m01: number,
+    m10: number,
     m11: number,
   ): Mat2 {
     this.m00 = m00;
-    this.m10 = m10;
     this.m01 = m01;
+    this.m10 = m10;
     this.m11 = m11;
     return this;
   }
   setScalar(scalar: number): Mat2 {
     this.m00 = scalar;
-    this.m10 = scalar;
     this.m01 = scalar;
+    this.m10 = scalar;
     this.m11 = scalar;
     return this;
   }
   copy(v: Mat2): Mat2 {
     this.m00 = v.m00;
-    this.m10 = v.m10;
     this.m01 = v.m01;
+    this.m10 = v.m10;
     this.m11 = v.m11;
     return this;
   }
   identity(): Mat2 {
     this.m00 = 1.0;
-    this.m10 = 0.0;
     this.m01 = 0.0;
+    this.m10 = 0.0;
     this.m11 = 1.0;
     return this;
   }
   transpose(): Mat2 {
-    const m10 = this.m10;
     const m01 = this.m01;
-    this.m10 = m10;
+    const m10 = this.m10;
     this.m01 = m01;
+    this.m10 = m10;
     return this;
   }
   multiply(v: Mat2): Mat2 {
     const m00 = this.m00;
-    const m10 = this.m10;
     const m01 = this.m01;
+    const m10 = this.m10;
     const m11 = this.m11;
-    this.m00 = m00 * v.m00 + m01 * v.m10;
-    this.m10 = m10 * v.m00 + m11 * v.m10;
-    this.m01 = m00 * v.m01 + m01 * v.m11;
-    this.m11 = m10 * v.m01 + m11 * v.m11;
+    this.m00 = m00 * v.m00 + m10 * v.m01;
+    this.m01 = m01 * v.m00 + m11 * v.m01;
+    this.m10 = m00 * v.m10 + m10 * v.m11;
+    this.m11 = m01 * v.m10 + m11 * v.m11;
     return this;
   }
   scale(v: Vec2): Mat2 {
     this.m00 *= v.x;
-    this.m10 *= v.x;
-    this.m01 *= v.y;
+    this.m01 *= v.x;
+    this.m10 *= v.y;
     this.m11 *= v.y;
     return this;
   }
   rotate(radian: number): Mat2 {
     const m00 = this.m00;
-    const m10 = this.m10;
     const m01 = this.m01;
+    const m10 = this.m10;
     const m11 = this.m11;
     const sin = Math.sin(radian);
     const cos = Math.cos(radian);
-    this.m00 = m00 *  cos + m01 * sin;
-    this.m10 = m10 *  cos + m11 * sin;
-    this.m01 = m00 * -sin + m01 * cos;
-    this.m11 = m10 * -sin + m11 * cos;
+    this.m00 = m00 *  cos + m10 * sin;
+    this.m01 = m01 *  cos + m11 * sin;
+    this.m10 = m00 * -sin + m10 * cos;
+    this.m11 = m01 * -sin + m11 * cos;
     return this;
   }
   invert(): Mat2 {
     const m00 = this.m00;
-    const m10 = this.m10;
     const m01 = this.m01;
+    const m10 = this.m10;
     const m11 = this.m11;
     const determinant = this.determinant();
     if(determinant !== 0.0) {
       const inverseDeterminant = 1.0 / determinant;
       this.m00 =  m11 * inverseDeterminant;
-      this.m10 = -m10 * inverseDeterminant;
       this.m01 = -m01 * inverseDeterminant;
+      this.m10 = -m10 * inverseDeterminant;
       this.m11 =  m00 * inverseDeterminant;
     }
     return this;
@@ -126,15 +126,15 @@ export class Mat2 {
     const sin = Math.sin(radian);
     const cos = Math.cos(radian);
     this.m00 =  cos;
-    this.m10 =  sin;
-    this.m01 = -sin;
+    this.m01 =  sin;
+    this.m10 = -sin;
     this.m11 =  cos;
     return this;
   }
   fromScalling(scale: number): Mat2 {
     this.m00 = scale;
+    this.m00 = 0.0;
     this.m10 = 0.0;
-    this.m01 = 0.0;
     this.m11 = scale;
     return this;
   }
@@ -143,8 +143,8 @@ export class Mat2 {
   clone(): Mat2 {
     return new Mat2(
       this.m00,
-      this.m10,
       this.m01,
+      this.m10,
       this.m11,
     );
   }
@@ -161,18 +161,18 @@ export class Mat2 {
     return this.clone().invert();
   }
   determinant(): number {
-    return this.m00 * this.m11 - this.m01 * this.m10;
+    return this.m00 * this.m11 - this.m10 * this.m01;
   }
   equals(v: Mat2): boolean {
     return (
       this.m00 === v.m00 &&
-      this.m10 === v.m10 &&
       this.m01 === v.m01 &&
+      this.m10 === v.m10 &&
       this.m11 === v.m11
     );
   }
   toArray(): number[] {
-    return [this.m00, this.m10, this.m10, this.m11];
+    return [this.m00, this.m01, this.m10, this.m11];
   }
   toString(separator?: string): string {
     return this.toArray().join(separator);
