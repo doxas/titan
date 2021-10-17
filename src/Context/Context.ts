566d53bba98e78d7aeb2843a6fbe02b590f9b439
doxas <eizanco@gmail.com>
2021-10-18 01:40:20 +0900

    [wip] initialize context

--- src/Context/Context.ts
+++ src/Context/Context.ts
@@ -1,5 +1,5 @@
 
-// import { XXX } from "";
+import { Logger } from "../Common/logger";
 
 export class Context {
   /** static getter ========================================================= */
@@ -11,9 +11,19 @@ export class Context {
   /** setter ================================================================ */
 
   /** property ============================================================== */
+  canvas: HTMLCanvasElement;
+  gpu: GPU;
+  adapter: GPUAdapter;
+  device: GPUDevice;
+  queue: GPUQueue;
 
   /** constructor =========================================================== */
-  constructor() {
+  constructor(canvas?: HTMLCanvasElement) {
+    this.gpu = navigator.gpu;
+    if(this.gpu == null) {
+      Logger.log('webgpu not support');
+    }
+    this.canvas = canvas != null ? canvas : document.createElement('canvas');
   }
 
   /** chain method ========================================================== */
