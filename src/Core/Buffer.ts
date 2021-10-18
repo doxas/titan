33fd228cef8e722991e91b025471810b5699b907
doxas <eizanco@gmail.com>
2021-10-19 00:41:09 +0900

    add class for buffer

---
+++ src/Core/Buffer.ts
@@ -0,0 +1,39 @@
+
+import { Descriptor } from '../Creator/Descriptor';
+
+export class Buffer {
+  /** static getter ========================================================= */
+
+  /** static method ========================================================= */
+
+  /** getter ================================================================ */
+
+  /** setter ================================================================ */
+
+  /** property ============================================================== */
+  device: GPUDevice;
+
+  /** constructor =========================================================== */
+  constructor(device: GPUDevice) {
+    this.device = device;
+  }
+
+  /** chain method ========================================================== */
+
+  /** method ================================================================ */
+  create(arrayBuffer: Float32Array | Uint16Array, usage: GPUBufferUsageFlags): GPUBuffer {
+    const size = (arrayBuffer.byteLength + 3) & ~3;
+    const mappedAtCreation = true;
+    const descriptor = Descriptor.bufferDescriptor(size, usage, mappedAtCreation);
+    const buffer = this.device.createBuffer(descriptor);
+
+    // TODO: why do this?
+    const writeArray = arrayBuffer instanceof Uint16Array ? new Uint16Array(buffer.getMappedRange()) : new Float32Array(buffer.getMappedRange());
+    writeArray.set(arrayBuffer);
+
+    buffer.unmap();
+    return buffer;
+  }
+}
+
+
