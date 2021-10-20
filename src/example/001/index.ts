1ad6fd5f537129b7432b668b7539acd204d7657d
doxas <eizanco@gmail.com>
2021-10-20 23:52:47 +0900

    [wip] add example

---
+++ src/example/001/index.ts
@@ -0,0 +1,15 @@
+
+const position = [
+   0.0,  0.5,  0.0,
+   0.5, -0.5,  0.0,
+  -0.5, -0.5,  0.0,
+];
+const color = [
+  1.0, 0.0, 0.0,
+  0.0, 1.0, 0.0,
+  0.0, 0.0, 1.0,
+];
+const indices = [0, 1, 2];
+
+// @ts-ignore
+console.log(window.TITAN, position);
