4ace12bf94e28778167caeef623bb5d846bbe519
doxas <eizanco@gmail.com>
2021-10-24 05:52:18 +0900

    implement buffer

--- src/Common/Buffer.ts
+++ src/Common/Buffer.ts
@@ -1,4 +1,5 @@
 
+import { Pipeline } from '../Core/Pipeline';
 import { Base } from './Base';
 
 export class Buffer extends Base {
@@ -7,33 +8,62 @@ export class Buffer extends Base {
   /** static method ========================================================= */
 
   /** getter ================================================================ */
+  get data(): GPUBuffer {
+    return this._buffer;
+  }
 
   /** setter ================================================================ */
 
   /** property ============================================================== */
+  private _buffer: GPUBuffer;
+  private _array: Float32Array | Uint16Array;
+  private _flags: GPUBufferUsageFlags;
 
   /** constructor =========================================================== */
   constructor() {
     super();
+
+    this._buffer = null;
+    this._array = null;
+    this._flags = GPUBufferUsage.VERTEX;
   }
 
   /** chain method ========================================================== */
+  set(arrayBuffer: Float32Array | Uint16Array, usage: GPUBufferUsageFlags): this {
+    this._array = arrayBuffer;
+    this._flags = usage;
+    this._changed = true;
+
+    return this;
+  }
+  destroy(): this {
+    if (this._buffer != null) {
+      this._buffer.destroy();
+      this._buffer = null;
+    }
+    this._changed = false;
+
+    return this;
+  }
 
   /** method ================================================================ */
-  create(arrayBuffer: Float32Array | Uint16Array, usage: GPUBufferUsageFlags): GPUBuffer {
-    const size = (arrayBuffer.byteLength + 3) & ~3;
-    const mappedAtCreation = true;
-    const descriptor = Descriptor.bufferDescriptor(size, usage, mappedAtCreation);
-    const buffer = this.device.createBuffer(descriptor);
+  createByPipeline(pipeline: Pipeline): void {
+    if (!this._changed) {return;}
 
-    // TODO: why do this?
-    const writeArray = arrayBuffer instanceof Uint16Array
-      ? new Uint16Array(buffer.getMappedRange())
-      : new Float32Array(buffer.getMappedRange());
-    writeArray.set(arrayBuffer);
+    this.destroy();
 
-    buffer.unmap();
-    return buffer;
+    const descriptor: GPUBufferDescriptor = {
+      size: (this._array.byteLength + 3) & ~3,
+      mappedAtCreation: true,
+      usage: this._flags,
+    };
+    this._buffer = pipeline.device.createBuffer(descriptor);
+
+    const writeArray = this._array instanceof Uint16Array
+      ? new Uint16Array(this._buffer.getMappedRange())
+      : new Float32Array(this._buffer.getMappedRange());
+    writeArray.set(this._array);
+    this._buffer.unmap();
   }
 }
 
