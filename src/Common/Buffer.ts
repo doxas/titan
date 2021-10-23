a47e1d3ab1939a72d5c61896a040475f8ba11d93
doxas <eizanco@gmail.com>
2021-10-24 06:39:53 +0900

    moidfy buffer

--- src/Common/Buffer.ts
+++ src/Common/Buffer.ts
@@ -2,6 +2,11 @@
 import { Pipeline } from '../Core/Pipeline';
 import { Base } from './Base';
 
+export interface IBuffer {
+  typedArray: Float32Array | Uint16Array;
+  usage: GPUBufferUsageFlags;
+}
+
 export class Buffer extends Base {
   /** static getter ========================================================= */
 
@@ -11,29 +16,28 @@ export class Buffer extends Base {
   get data(): GPUBuffer {
     return this._buffer;
   }
+  get usage(): GPUBufferUsageFlags {
+    return this._usage;
+  }
 
   /** setter ================================================================ */
 
   /** property ============================================================== */
-  private _buffer: GPUBuffer;
   private _array: Float32Array | Uint16Array;
   private _usage: GPUBufferUsageFlags;
+  private _buffer: GPUBuffer;
 
   /** constructor =========================================================== */
-  constructor() {
+  constructor(option: IBuffer) {
     super();
-
-    this._buffer = null;
-    this._array = null;
-    this._usage = GPUBufferUsage.VERTEX;
+    this.set(option);
   }
 
   /** chain method ========================================================== */
-  set(arrayBuffer: Float32Array | Uint16Array, usage: GPUBufferUsageFlags): this {
-    this._array = arrayBuffer;
-    this._usage = usage;
+  set(option: IBuffer): this {
+    this._array = option.typedArray;
+    this._usage = option.usage;
     this._changed = true;
-
     return this;
   }
   destroy(): this {
@@ -42,16 +46,13 @@ export class Buffer extends Base {
       this._buffer = null;
     }
     this._changed = false;
-
     return this;
   }
 
   /** method ================================================================ */
   createByPipeline(pipeline: Pipeline): void {
     if (!this._changed) {return;}
-
     this.destroy();
-
     const descriptor: GPUBufferDescriptor = {
       size: (this._array.byteLength + 3) & ~3,
       mappedAtCreation: true,
