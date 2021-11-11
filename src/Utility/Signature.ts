c2f12f70a172192163762332f48d0b476bbd1a38
doxas <eizanco@gmail.com>
2021-11-11 16:59:33 +0900

    add traverse type

--- src/Utility/Signature.ts
+++ src/Utility/Signature.ts
@@ -1,4 +1,10 @@
 
+import { Base3D } from "../Common/Base3D";
+
 export type ForEach = {
-  (currentValuemm: any, index: number, array: []): void;
+  (currentValuemm: any, index?: number, array?: []): void;
+};
+
+export type Traverse = {
+  (base: Base3D): void;
 };
