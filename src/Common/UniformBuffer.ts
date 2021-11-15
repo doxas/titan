4999dc3ec6907e5e703feab9a794a7eccb35f5b6
doxas <eizanco@gmail.com>
2021-11-15 22:17:57 +0900

    add uniform buffer class

---
+++ src/Common/UniformBuffer.ts
@@ -0,0 +1,55 @@
+
+import { Base } from './Base';
+import { Buffer } from './Buffer';
+
+export interface IUniformBuffer {
+  data: number[] | Float32Array;
+}
+
+export class UniformBuffer extends Base {
+  /** static getter ========================================================= */
+
+  /** static method ========================================================= */
+
+  /** getter ================================================================ */
+
+  /** setter ================================================================ */
+
+  /** property ============================================================== */
+  source: number[] | Float32Array;
+  buffer: Buffer;
+
+  /** constructor =========================================================== */
+  constructor(option: IUniformBuffer) {
+    super();
+    this.set(option);
+  }
+
+  /** chain method ========================================================== */
+  set(option: IUniformBuffer): this {
+    this.source = option.data;
+    this._changed = true;
+    return this;
+  }
+  destroy(): this {
+    if (this.buffer != null) {
+      this.buffer.destroy();
+      this.buffer = null;
+    }
+    this._changed = false;
+    return this;
+  }
+  create(): this {
+    if (!this._changed) {return;}
+    this.destroy();
+    this.buffer = new Buffer({
+      typedArray: Float32Array.from(this.source),
+      usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+    return this;
+  }
+
+  /** method ================================================================ */
+}
+
+
